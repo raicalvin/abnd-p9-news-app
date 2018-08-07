3e25384b2b5be9089fa75ea1cd07664f6356adc6
Calvin Rai <raicalvin@gmail.com>
2018-08-06 20:07:11 -0700

    Added placeholder feature items in ArrayList.

--- app/src/main/java/com/example/newsapp/MainActivity.java
+++ app/src/main/java/com/example/newsapp/MainActivity.java
@@ -3,11 +3,24 @@ package com.example.newsapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ArrayList<Feature> features = new ArrayList<>();
+        features.add(new Feature("Sports", "Kobe Comes Back", "August 9, 2017", 1));
+        features.add(new Feature("Technology", "Something about iPhone", "March 10, 2017", 3));
+        features.add(new Feature("Technology", "Android and iOS Merging", "December 18, 2017", 7));
+        features.add(new Feature("Health", "Zombie Virus Spreading in Southern Texas", "January 23, 2017", 19));
+        features.add(new Feature("Sports", "Warriors Coming to Oakland", "October 1, 2017", 21));
+        features.add(new Feature("Shopping", "Black Friday Deals Happening Right Now", "April 7, 2017", 45));
+
+
+
     }
 }
