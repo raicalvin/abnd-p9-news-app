76bd7e74c469b2d14a023ac9a84914d40f961d0b
Calvin Rai <raicalvin@gmail.com>
2018-08-26 19:57:22 -0700

    Updated FeatureNewsLoader with starter code content.

--- app/src/main/java/com/example/newsapp/FeatureNewsLoader.java
+++ app/src/main/java/com/example/newsapp/FeatureNewsLoader.java
@@ -1,8 +1,24 @@
 package com.example.newsapp;
 
 import android.content.AsyncTaskLoader;
+import android.content.Context;
 
 import java.util.List;
 
 public class FeatureNewsLoader extends AsyncTaskLoader<List<Feature>> {
+
+    public FeatureNewsLoader(Context context, String url) {
+        super(context);
+        // TODO: Finish implementing constructor
+    }
+
+    @Override
+    protected void onStartLoading() {
+        forceLoad();
+    }
+
+    @Override
+    public List<Feature> loadInBackground() {
+        // TODO: Implement this method
+    }
 }
