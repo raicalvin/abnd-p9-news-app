57c6073c071fad6bae140a42cfd4de8c65d983c9
Calvin Rai <raicalvin@gmail.com>
2018-09-03 10:44:38 -0700

    Fixed bug that was not showing the Minimum Stories setting activity.

--- app/src/main/java/com/example/newsapp/SettingsActivity.java
+++ app/src/main/java/com/example/newsapp/SettingsActivity.java
@@ -9,10 +9,17 @@ public class SettingsActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_settings);
+        setContentView(R.layout.settings_activity);
     }
 
     public static class NewsPreferenceFragment extends PreferenceFragment {
+
+
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            addPreferencesFromResource(R.xml.settings_main);
+        }
 
     }
 }
